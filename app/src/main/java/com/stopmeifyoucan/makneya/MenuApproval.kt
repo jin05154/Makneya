90466fad3ae123151b824e47eaacf49b0a828e94
Yumin Lee <dorayuminlee@gmail.com>
2021-06-08 01:46:52 +0900

    Menu Approval 식당 리스트

--- app/src/main/java/com/stopmeifyoucan/makneya/MenuApproval.kt
+++ app/src/main/java/com/stopmeifyoucan/makneya/MenuApproval.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.cardview.widget.CardView
-import android.widget.LinearLayout.LayoutParams
 import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
@@ -30,51 +29,53 @@ class MenuApproval : AppCompatActivity() {
         val mapFragment = MapFragment.newInstance(options)
         fm.beginTransaction().add(R.id.map, mapFragment).commit()
 
-        // Initialize a new LayoutParams instance, CardView width and height
-        val layoutParams = RelativeLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT, // CardView width
-            ViewGroup.LayoutParams.WRAP_CONTENT // CardView height
-        )
+        for(i: Int in 1..5) {
+            // Initialize a new LayoutParams instance, CardView width and height
+            val layoutParams = RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, // CardView width
+                ViewGroup.LayoutParams.WRAP_CONTENT // CardView height
+            )
 
-        layoutParams.setMargins(16,16,16,50)
+            layoutParams.setMargins(16,16,16,50)
 
-        // Initialize a new CardView instance
-        val menuCardView = CardView(this)
+            // Initialize a new CardView instance
+            val menuCardView = CardView(this)
 
-        // Set bottom margin for card view
-        //layoutParams.bottomMargin = 50
+            // Set bottom margin for card view
+            //layoutParams.bottomMargin = 50
 
-        // Set the card view layout params
-        menuCardView.layoutParams = layoutParams
+            // Set the card view layout params
+            menuCardView.layoutParams = layoutParams
 
-        // Set the card view corner radius
-        menuCardView.radius = 12F
+            // Set the card view corner radius
+            menuCardView.radius = 12F
 
-        // Set the card view content padding
-        menuCardView.setContentPadding(25,25,25,25)
+            // Set the card view content padding
+            menuCardView.setContentPadding(25,25,25,25)
 
-        // Set the card view background color
-        menuCardView.setCardBackgroundColor(Color.LTGRAY)
+            // Set the card view background color
+            menuCardView.setCardBackgroundColor(Color.LTGRAY)
 
-        // Set card view elevation
-        menuCardView.cardElevation = 8F
+            // Set card view elevation
+            menuCardView.cardElevation = 8F
 
-        // Set card view maximum elevation
-        menuCardView.maxCardElevation = 12F
+            // Set card view maximum elevation
+            menuCardView.maxCardElevation = 12F
 
-        // Set a click listener for card view
-        menuCardView.setOnClickListener{
-            Toast.makeText(
-                applicationContext,
-                "식당 상세보기",
-                Toast.LENGTH_SHORT).show()
+            // Set a click listener for card view
+            menuCardView.setOnClickListener{
+                Toast.makeText(
+                    applicationContext,
+                    "식당 상세보기",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            // Add LinearLayout to the CardView
+            menuCardView.addView(generateCardView())
+
+            // Finally, add the CardView in root layout
+            root_layout.addView(menuCardView)
         }
-
-        // Add an ImageView to the CardView
-        menuCardView.addView(generateCardView())
-
-        // Finally, add the CardView in root layout
-        root_layout.addView(menuCardView)
     }
 
     // Custom method to generate an image view
