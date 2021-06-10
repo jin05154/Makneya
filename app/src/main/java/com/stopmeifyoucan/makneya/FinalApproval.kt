7a9667e13ce50cf5c13e0684224a98c35d0ebd25
Yumin <dorayuminlee@gmail.com>
2021-06-10 09:45:22 +0900

    Final Approval (firebase name)

--- app/src/main/java/com/stopmeifyoucan/makneya/FinalApproval.kt
+++ app/src/main/java/com/stopmeifyoucan/makneya/FinalApproval.kt
@@ -3,6 +3,8 @@ package com.stopmeifyoucan.makneya
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.stopmeifyoucan.makneya.Data.InDB
 import kotlinx.android.synthetic.main.activity_final_approval.*
 import java.text.SimpleDateFormat
@@ -14,8 +16,9 @@ class FinalApproval : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_final_approval)
 
-        finalName.text = InDB.prefs.getString("name", "")
-        consultTextName2.text = InDB.prefs.getString("name", "")
+        //finalName.text = InDB.prefs.getString("name", "")
+        //consultTextName2.text = InDB.prefs.getString("name", "")
+        getUserProfile()
 
         val currentDateTime = Calendar.getInstance().time
         var dateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA).format(currentDateTime)
@@ -30,6 +33,16 @@ class FinalApproval : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
+        }
+    }
+
+    private fun getUserProfile() {
+        val user = Firebase.auth.currentUser
+        user?.let {
+            val name = user.displayName
+
+            finalName.text = name
+            consultTextName2.text = name
         }
     }
 }
