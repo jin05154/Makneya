1d23b4f48808c890700ee660caaef69b5bf6d0d7
Yumin <dorayuminlee@gmail.com>
2021-09-02 03:01:38 +0900

    [feat] 간편요리 탭 기능 구현 시작 (activity)

--- app/src/main/java/com/stopmeifyoucan/makneya/navigation/TabRecipe.kt
+++ app/src/main/java/com/stopmeifyoucan/makneya/navigation/TabRecipe.kt
@@ -1,19 +1,49 @@
 package com.stopmeifyoucan.makneya.navigation
+
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 import com.stopmeifyoucan.makneya.R
 
-class TabRecipe : Fragment() {
+class TabRecipe : YouTubeBaseActivity() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.tab_youtube, container, false)
+    val videoId = "F-KjYNmsi0U"
 
-    companion object {
-        fun newInstance(): TabRecipe =
-            TabRecipe()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tab_youtube)
+
+        val youtubeView = findViewById<YouTubePlayerView>(R.id.youtube_view)
+        youtubeView.initialize("develop", object : YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider,
+                player: YouTubePlayer,
+                wasRestored: Boolean
+            ) {
+                if (!wasRestored) {
+                    player.loadVideo(videoId)
+                }
+
+                player.setPlayerStateChangeListener(object :
+                    YouTubePlayer.PlayerStateChangeListener {
+                    override fun onAdStarted() {}
+                    override fun onLoading() {}
+                    override fun onVideoStarted() {}
+                    override fun onVideoEnded() {}
+                    override fun onError(p0: YouTubePlayer.ErrorReason) {}
+                    override fun onLoaded(videoId: String) {
+                        player.play()
+                    }
+                })
+            }
+
+            override fun onInitializationFailure(
+                provider: YouTubePlayer.Provider?,
+                result: YouTubeInitializationResult? ) {
+
+            }
+        })
     }
-
 }
